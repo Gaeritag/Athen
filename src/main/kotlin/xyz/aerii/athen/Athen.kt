036dafcafbf40756a0390694f1d2b810f44d12fb
skies-starred <github@starred.foo>
2026-07-29 20:07:55 +0530

    bump(version): 0.2.0

--- src/main/kotlin/xyz/aerii/athen/Athen.kt
+++ src/main/kotlin/xyz/aerii/athen/Athen.kt
@@ -27,7 +27,7 @@ import xyz.aerii.library.utils.literal
 import kotlin.time.Duration.Companion.hours
 
 object Athen : ClientModInitializer {
-    const val modVersion: String = /*$ mod_version*/ "0.1.9"
+    const val modVersion: String = /*$ mod_version*/ "0.2.0"
     const val modId: String = /*$ mod_id*/ "athen"
     const val modName: String = /*$ mod_name*/ "Athen"
     const val discordUrl: String = "https://discord.gg/DB5S3DjQVa"
